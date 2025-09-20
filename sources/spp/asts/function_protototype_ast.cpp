b997e1986d9ea936d1c327752f860daf55e393c0
SamG101-Developer <samuelgardner101@gmail.com>
2025-09-20 21:06:43 +0100

    Add missing scope escape

--- sources/spp/asts/function_protototype_ast.cpp
+++ sources/spp/asts/function_protototype_ast.cpp
@@ -302,6 +302,7 @@ auto spp::asts::FunctionPrototypeAst::stage_4_qualify_types(
     sm->move_to_next_scope();
     generic_param_group->stage_4_qualify_types(sm, meta);
     impl->stage_4_qualify_types(sm, meta);
+    sm->move_out_of_current_scope();
 }
 
 
