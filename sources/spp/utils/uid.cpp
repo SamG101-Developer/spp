0302598d63e9993a0b935c9b321b01cb8d135d76
SamG101-Developer <samuelgardner101@gmail.com>
2025-12-27 23:59:17 +0000

    Make uid use ast pointer

--- sources/spp/utils/uid.cpp
+++ sources/spp/utils/uid.cpp
@@ -2,10 +2,10 @@ module spp.utils.uid;
 import spp.asts.ast;
 
 
-auto spp::utils::generate_uid(asts::Ast const *)
+auto spp::utils::generate_uid(asts::Ast const *ast)
     -> std::string {
     static std::size_t uid_counter = 0;
-    return "$" + std::to_string(uid_counter++);
+    return "$" + std::to_string(reinterpret_cast<std::uintptr_t>(ast)) + "_" + std::to_string(uid_counter++);
 }
 
 
