63a4dc7f6337c661c5fc4f3c3b1d9e02063fbd64
SamG101-Developer <samuelgardner101@gmail.com>
2026-06-14 16:33:03 +0100

    Fix bugs with aliases

--- sources/spp/asts/generic_argument_type_ast.cpp
+++ sources/spp/asts/generic_argument_type_ast.cpp
@@ -6,6 +6,9 @@ import spp.analyse.scopes.scope;
 import spp.analyse.scopes.scope_manager;
 import spp.analyse.scopes.symbols;
 import spp.asts.convention_ast;
+import spp.asts.generic_argument_group_ast;
+import spp.asts.generic_argument_comp_ast;
+import spp.asts.identifier_ast;
 import spp.asts.type_ast;
 import spp.asts.type_identifier_ast;
 import spp.asts.meta.compiler_meta_data;
@@ -27,26 +30,18 @@ auto spp::asts::GenericArgumentTypeAst::Stage4_QualifyTypes(
     ScopeManager *sm,
     CompilerMetaData *meta)
     -> void {
-    // If the generic arg is generic itself, from type aliasing, do nothing.
-    // if (meta->alias_qualifier_scope != nullptr) {
-    //     const auto sym = meta->alias_qualifier_scope->GetTypeSymbol(val, true);
-    //     if (sym and sym->IsGeneric) { return; }
-    // }
-
-    // Qualify the type value without generics, then re-add the generics.
     Val->Stage4_QualifyTypes(sm, meta);
-
-    // The value could be a generic from another scope, so only modify if it exists.
-    const auto raw = Val->WithoutGenerics();
-    const auto sym = sm->CurrentScope->GetTypeSymbol(raw);
-    if (sym and sym->AliasStmt != nullptr) {
-        auto temp = sym->FqName()->WithConvention(AstClone(Val->GetConvention()));
-        Val = std::move(temp);
+    const auto sym = sm->CurrentScope->GetTypeSymbol(Val, true);
+    if (sym and not sym->AliasStmt) {
+        const auto fq = sym ? sym->FqName() : nullptr;
+        Val = fq ? fq : Val;
+        return;
     }
-    else if (sym != nullptr) {
-        auto temp = sym->FqName()->WithConvention(AstClone(Val->GetConvention()));
-        temp = temp->WithGenerics(std::move(Val->TypeParts().Back()->GnArgGroup));
-        Val = std::move(temp);
+
+    const auto sym2 = sm->CurrentScope->GetTypeSymbol(Val->WithoutGenerics(), true);
+    if (sym2 && !sym2->AliasStmt) {
+        const auto fq = sym2->FqName();
+        Val = fq->WithGenerics(std::move(Val->TypeParts().Back()->GnArgGroup))->WithConvention(AstClone(Val->GetConvention()));
     }
 }
 
