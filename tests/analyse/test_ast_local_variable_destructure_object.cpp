aabb86e9ac56b402d410cd67daba91ab2ce16019
SamG101-Developer <samuelgardner101@gmail.com>
2026-07-30 21:09:15 +0100

    Add destructure bug to test suites

--- tests/analyse/test_ast_local_variable_destructure_object.cpp
+++ tests/analyse/test_ast_local_variable_destructure_object.cpp
@@ -1,9 +1,9 @@
 #include "../test_macros.hpp"
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_invalid_multiple_multi_skip,
-    SppMultipleRestPatternsError, R"(
+  LocalVariableDestructureObjectAst,
+  test_invalid_multiple_multi_skip,
+  SppMultipleRestPatternsError, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -15,9 +15,9 @@ SPP_TEST_SHOULD_FAIL_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_invalid_array_missing_attribute,
-    SppArgumentMissingError, R"(
+  LocalVariableDestructureObjectAst,
+  test_invalid_array_missing_attribute,
+  SppArgumentMissingError, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -29,9 +29,9 @@ SPP_TEST_SHOULD_FAIL_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_invalid_array_invalid_attribute,
-    SppIdentifierUnknownError, R"(
+  LocalVariableDestructureObjectAst,
+  test_invalid_array_invalid_attribute,
+  SppIdentifierUnknownError, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -43,8 +43,8 @@ SPP_TEST_SHOULD_FAIL_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_valid_simple, R"(
+  LocalVariableDestructureObjectAst,
+  test_valid_simple, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -56,8 +56,8 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_valid_with_multi_skip, R"(
+  LocalVariableDestructureObjectAst,
+  test_valid_with_multi_skip, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -70,9 +70,9 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_invalid_with_bound_multi_skip,
-    SppVariableObjectDestructureWithBoundRestPatternError, R"(
+  LocalVariableDestructureObjectAst,
+  test_invalid_with_bound_multi_skip,
+  SppVariableObjectDestructureWithBoundRestPatternError, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -85,8 +85,8 @@ SPP_TEST_SHOULD_FAIL_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_valid_nested_array, R"(
+  LocalVariableDestructureObjectAst,
+  test_valid_nested_array, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -101,8 +101,8 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_valid_nested_tuple, R"(
+  LocalVariableDestructureObjectAst,
+  test_valid_nested_tuple, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -117,8 +117,8 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_valid_nested_object, R"(
+  LocalVariableDestructureObjectAst,
+  test_valid_nested_object, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -140,8 +140,8 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_valid_assign_to_uninitialized_parts, R"(
+  LocalVariableDestructureObjectAst,
+  test_valid_assign_to_uninitialized_parts, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -155,9 +155,9 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_invalid_explicit_type_missing_attribute,
-    SppArgumentMissingError, R"(
+  LocalVariableDestructureObjectAst,
+  test_invalid_explicit_type_missing_attribute,
+  SppArgumentMissingError, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -170,9 +170,9 @@ SPP_TEST_SHOULD_FAIL_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_invalid_explicit_type_too_small,
-    SppIdentifierUnknownError, R"(
+  LocalVariableDestructureObjectAst,
+  test_invalid_explicit_type_too_small,
+  SppIdentifierUnknownError, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -184,9 +184,9 @@ SPP_TEST_SHOULD_FAIL_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_invalid_assign_to_individual_part_wrong_type,
-    SppTypeMismatchError, R"(
+  LocalVariableDestructureObjectAst,
+  test_invalid_assign_to_individual_part_wrong_type,
+  SppTypeMismatchError, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -200,9 +200,9 @@ SPP_TEST_SHOULD_FAIL_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_invalid_target_with_convention_ref,
-    SppTypeMismatchError, R"(
+  LocalVariableDestructureObjectAst,
+  test_invalid_target_with_convention_ref,
+  SppTypeMismatchError, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -216,9 +216,9 @@ SPP_TEST_SHOULD_FAIL_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureObjectAst,
-    test_invalid_target_with_convention_mut,
-    SppTypeMismatchError, R"(
+  LocalVariableDestructureObjectAst,
+  test_invalid_target_with_convention_mut,
+  SppTypeMismatchError, R"(
     cls Point {
         !public x: S32
         !public y: S32
@@ -228,5 +228,136 @@ SPP_TEST_SHOULD_FAIL_SEMANTIC(
     fun f(p: &mut Point) -> Void {
         let Point(mut x, y, z) = p
         x = 10
+    }
+)");
+
+SPP_TEST_SHOULD_PASS_SEMANTIC(
+  LocalVariableDestructureObjectAst_MaterializeRhs,
+  test_valid_scoped_value_case, R"(
+    cls Point {
+        !public x: S32
+        !public y: S32
+    }
+
+    fun f(a: S32) -> Void {
+        let Point(x, y) = case a == 1 { Point(x=0, y=9) } else { Point(x=1, y=8) }
+        let done = false
+        loop done { }
+    }
+)");
+
+SPP_TEST_SHOULD_PASS_SEMANTIC(
+  LocalVariableDestructureObjectAst_MaterializeRhs,
+  test_valid_scoped_value_case_chain, R"(
+    cls Point {
+        !public x: S32
+        !public y: S32
+    }
+
+    fun f(a: S32) -> Void {
+        let Point(x, y) = case a == 1 { Point(x=0, y=9) }
+        else case a == 2 { Point(x=1, y=8) }
+        else case a == 3 { Point(x=2, y=7) }
+        else { Point(x=3, y=6) }
+        let done = false
+        loop done { }
+    }
+)");
+
+SPP_TEST_SHOULD_PASS_SEMANTIC(
+  LocalVariableDestructureObjectAst_MaterializeRhs,
+  test_valid_scoped_value_inner_scope, R"(
+    cls Point {
+        !public x: S32
+        !public y: S32
+    }
+
+    fun f() -> Void {
+        let Point(x, y) = {
+            let a = 1
+            Point(x=a, y=9)
+        }
+        let done = false
+        loop done { }
+    }
+)");
+
+SPP_TEST_SHOULD_PASS_SEMANTIC(
+  LocalVariableDestructureObjectAst_MaterializeRhs,
+  test_valid_scoped_value_moved_once, R"(
+    cls Pair {
+        !public a: Str
+        !public b: Str
+    }
+
+    fun f(c: Bool) -> Void {
+        let s = Str::from("a")
+        let Pair(a, b) = case c { Pair(a=s, b=Str::from("b")) } else { Pair(a=s, b=Str::from("d")) }
+    }
+)");
+
+SPP_TEST_SHOULD_FAIL_SEMANTIC(
+  LocalVariableDestructureObjectAst_MaterializeRhs,
+  test_invalid_scoped_value_already_moved,
+  SppUninitializedMemoryUseError, R"(
+    cls Pair {
+        !public a: Str
+        !public b: Str
+    }
+
+    fun f(c: Bool) -> Void {
+        let s = Str::from("a")
+        let t = s
+        let Pair(a, b) = case c { Pair(a=s, b=Str::from("b")) } else { Pair(a=s, b=Str::from("d")) }
+    }
+)");
+
+SPP_TEST_SHOULD_PASS_SEMANTIC(
+  LocalVariableDestructureObjectAst_MaterializeRhs,
+  test_valid_function_call_value, R"(
+    cls Pair {
+        !public a: Str
+        !public b: Str
+    }
+
+    fun g() -> Pair {
+        ret Pair(a=Str::from("a"), b=Str::from("b"))
+    }
+
+    fun f() -> Void {
+        let Pair(a, b) = g()
+        let done = false
+        loop done { }
+    }
+)");
+
+SPP_TEST_SHOULD_PASS_SEMANTIC(
+  LocalVariableDestructureObjectAst_MaterializeRhs,
+  test_valid_place_value_keeps_partial_moves, R"(
+    cls Pair {
+        !public a: Str
+        !public b: Str
+    }
+
+    fun f() -> Void {
+        let p = Pair(a=Str::from("a"), b=Str::from("b"))
+        let Pair(a, ..) = p
+        let b = p.b
+    }
+)");
+
+SPP_TEST_SHOULD_FAIL_SEMANTIC(
+  LocalVariableDestructureObjectAst_MaterializeRhs,
+  test_invalid_place_value_used_after_partial_move,
+  SppPartiallyInitializedMemoryUseError, R"(
+    cls Pair {
+        !public a: Str
+        !public b: Str
+    }
+
+    fun f() -> Void {
+        let p = Pair(a=Str::from("a"), b=Str::from("b"))
+        let Pair(a, ..) = p
+        let q = p
     }
 )");
--- tests/analyse/test_ast_local_variable_destructure_tuple.cpp
+++ tests/analyse/test_ast_local_variable_destructure_tuple.cpp
@@ -1,52 +1,52 @@
 #include "../test_macros.hpp"
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_invalid_non_array_value,
-    SppVariableTupleDestructureTupleTypeMismatchError, R"(
+  LocalVariableDestructureTupleAst,
+  test_invalid_non_array_value,
+  SppVariableTupleDestructureTupleTypeMismatchError, R"(
     fun f() -> Void {
         let (a, b) = 1
     }
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_invalid_multiple_multi_skip,
-    SppMultipleRestPatternsError, R"(
+  LocalVariableDestructureTupleAst,
+  test_invalid_multiple_multi_skip,
+  SppMultipleRestPatternsError, R"(
     fun f() -> Void {
         let (a, .., .., b) = [1, 2, 3, 4]
     }
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_invalid_array_missing_element,
-    SppVariableTupleDestructureTupleSizeMismatchError, R"(
+  LocalVariableDestructureTupleAst,
+  test_invalid_array_missing_element,
+  SppVariableTupleDestructureTupleSizeMismatchError, R"(
     fun f() -> Void {
         let (a, b) = (1, 2, 3)
     }
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_invalid_array_invalid_element,
-    SppVariableTupleDestructureTupleSizeMismatchError, R"(
+  LocalVariableDestructureTupleAst,
+  test_invalid_array_invalid_element,
+  SppVariableTupleDestructureTupleSizeMismatchError, R"(
     fun f() -> Void {
         let (a, b, c) = (1, 2)
     }
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_valid_simple, R"(
+  LocalVariableDestructureTupleAst,
+  test_valid_simple, R"(
     fun f() -> Void {
         let (a, b, c) = (1, 2, 3)
     }
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_valid_with_single_skip, R"(
+  LocalVariableDestructureTupleAst,
+  test_valid_with_single_skip, R"(
     fun f() -> Void {
         let (mut a, _, mut c) = (1, 2, 3)
         a = 5
@@ -55,16 +55,16 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_valid_with_multi_skip, R"(
+  LocalVariableDestructureTupleAst,
+  test_valid_with_multi_skip, R"(
     fun f() -> Void {
         let (a, .., c) = (1, 2, 3, 4, 5)
     }
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_valid_with_bound_multi_skip_start, R"(
+  LocalVariableDestructureTupleAst,
+  test_valid_with_bound_multi_skip_start, R"(
     fun f() -> Void {
         let (a, ..mut b, c) = (1, 2, 3, 4)
         b = (5, 6)
@@ -72,8 +72,8 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_valid_with_bound_multi_skip_middle, R"(
+  LocalVariableDestructureTupleAst,
+  test_valid_with_bound_multi_skip_middle, R"(
     fun f() -> Void {
         let (..mut a, b) = (1, 2, 3, 4)
         a = (5, 6, 7)
@@ -81,8 +81,8 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_valid_with_bound_multi_skip_end, R"(
+  LocalVariableDestructureTupleAst,
+  test_valid_with_bound_multi_skip_end, R"(
     fun f() -> Void {
         let (a, b, ..mut c) = (1, 2, 3, 4)
         c = (5, 6)
@@ -90,8 +90,8 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_valid_nested_array, R"(
+  LocalVariableDestructureTupleAst,
+  test_valid_nested_array, R"(
     fun f() -> Void {
         let t = (1, 2)
         let ((a, mut b), c) = (t, [3, 4])
@@ -100,8 +100,8 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_valid_nested_tuple, R"(
+  LocalVariableDestructureTupleAst,
+  test_valid_nested_tuple, R"(
     fun f() -> Void {
         let t = (1, Str::from("2"))
         let ((a, mut b), mut c) = (t, (3, Str::from("4")))
@@ -111,8 +111,8 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_valid_nested_object, R"(
+  LocalVariableDestructureTupleAst,
+  test_valid_nested_object, R"(
     cls Point {
         !public x: std::number::S32
         !public y: std::number::S32
@@ -126,8 +126,8 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_PASS_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_valid_assign_to_uninitialized_parts, R"(
+  LocalVariableDestructureTupleAst,
+  test_valid_assign_to_uninitialized_parts, R"(
     fun f() -> Void {
         let (x, y): (Bool, Bool)
         x = true
@@ -136,29 +136,120 @@ SPP_TEST_SHOULD_PASS_SEMANTIC(
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_invalid_explicit_type_too_big,
-    SppVariableTupleDestructureTupleSizeMismatchError, R"(
+  LocalVariableDestructureTupleAst,
+  test_invalid_explicit_type_too_big,
+  SppVariableTupleDestructureTupleSizeMismatchError, R"(
     fun f() -> Void {
         let (x, y): (Bool, Bool, Bool)
     }
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_invalid_explicit_type_too_small,
-    SppVariableTupleDestructureTupleSizeMismatchError, R"(
+  LocalVariableDestructureTupleAst,
+  test_invalid_explicit_type_too_small,
+  SppVariableTupleDestructureTupleSizeMismatchError, R"(
     fun f() -> Void {
         let (x, y): (Bool,)
     }
 )");
 
 SPP_TEST_SHOULD_FAIL_SEMANTIC(
-    LocalVariableDestructureTupleAst,
-    test_invalid_assign_to_individual_part_wrong_type,
-    SppTypeMismatchError, R"(
+  LocalVariableDestructureTupleAst,
+  test_invalid_assign_to_individual_part_wrong_type,
+  SppTypeMismatchError, R"(
     fun f() -> Void {
         let (x, y): (Bool, Bool)
         x = "hello world"
+    }
+)");
+
+SPP_TEST_SHOULD_PASS_SEMANTIC(
+  LocalVariableDestructureTupleAst_MaterializeRhs,
+  test_valid_scoped_value_case, R"(
+    fun f(a: S32) -> Void {
+        let (lo, hi) = case a == 1 { (0, 9) } else { (1, 8) }
+        let done = false
+        loop done { }
+    }
+)");
+
+SPP_TEST_SHOULD_PASS_SEMANTIC(
+  LocalVariableDestructureTupleAst_MaterializeRhs,
+  test_valid_scoped_value_case_chain, R"(
+    fun f(a: S32) -> Void {
+        let (lo, hi) = case a == 1 { (0, 9) }
+        else case a == 2 { (1, 8) }
+        else case a == 3 { (2, 7) }
+        else { (3, 6) }
+        let done = false
+        loop done { }
+    }
+)");
+
+SPP_TEST_SHOULD_PASS_SEMANTIC(
+  LocalVariableDestructureTupleAst_MaterializeRhs,
+  test_valid_scoped_value_inner_scope, R"(
+    fun f() -> Void {
+        let (lo, hi) = {
+            let a = 1
+            (a, 9)
+        }
+        let done = false
+        loop done { }
+    }
+)");
+
+SPP_TEST_SHOULD_PASS_SEMANTIC(
+  LocalVariableDestructureTupleAst_MaterializeRhs,
+  test_valid_scoped_value_moved_once, R"(
+    fun f(c: Bool) -> Void {
+        let s = Str::from("a")
+        let (a, b) = case c { (s, Str::from("b")) } else { (s, Str::from("d")) }
+    }
+)");
+
+SPP_TEST_SHOULD_FAIL_SEMANTIC(
+  LocalVariableDestructureTupleAst_MaterializeRhs,
+  test_invalid_scoped_value_already_moved,
+  SppUninitializedMemoryUseError, R"(
+    fun f(c: Bool) -> Void {
+        let s = Str::from("a")
+        let t = s
+        let (a, b) = case c { (s, Str::from("b")) } else { (s, Str::from("d")) }
+    }
+)");
+
+SPP_TEST_SHOULD_PASS_SEMANTIC(
+  LocalVariableDestructureTupleAst_MaterializeRhs,
+  test_valid_function_call_value, R"(
+    fun g() -> (Str, Str) {
+        ret (Str::from("a"), Str::from("b"))
+    }
+
+    fun f() -> Void {
+        let (a, b) = g()
+        let done = false
+        loop done { }
+    }
+)");
+
+SPP_TEST_SHOULD_PASS_SEMANTIC(
+  LocalVariableDestructureTupleAst_MaterializeRhs,
+  test_valid_place_value_keeps_partial_moves, R"(
+    fun f() -> Void {
+        let t = (Str::from("a"), Str::from("b"))
+        let (a, ..) = t
+        let b = t.1
+    }
+)");
+
+SPP_TEST_SHOULD_FAIL_SEMANTIC(
+  LocalVariableDestructureTupleAst_MaterializeRhs,
+  test_invalid_place_value_used_after_partial_move,
+  SppPartiallyInitializedMemoryUseError, R"(
+    fun f() -> Void {
+        let t = (Str::from("a"), Str::from("b"))
+        let (a, ..) = t
+        let u = t
     }
 )");
