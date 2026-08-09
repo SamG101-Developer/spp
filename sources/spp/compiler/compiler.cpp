5c72ffa8322abf26b8f0c021b304c04edc756fa1
SamG101-Developer <samuelgardner101@gmail.com>
2026-08-09 17:13:54 +0100

    Only do stage 9 for unit tests

--- sources/spp/compiler/compiler.cpp
+++ sources/spp/compiler/compiler.cpp
@@ -71,8 +71,10 @@ auto spp::compiler::Compiler::Compile() -> void {
     m_boot->Stage7_AnalyseSemantics(**ps++, *m_modules, is_exe, m_scope_manager.get());
     m_boot->Stage8_CheckMemory(**ps++, *m_modules, m_scope_manager.get());
     m_boot->Stage9_CompTimeResolve(**ps++, *m_modules, m_scope_manager.get());
-    m_boot->Stage10_PreCodeGen(**ps++, *m_modules, m_scope_manager.get());
-    m_boot->Stage11_CodeGen(**ps++, *m_modules, m_scope_manager.get());
+    if (not m_for_unit_tests) {
+      m_boot->Stage10_PreCodeGen(**ps++, *m_modules, m_scope_manager.get());
+      m_boot->Stage11_CodeGen(**ps++, *m_modules, m_scope_manager.get());
+    }
 #ifdef NDEBUG
   }
   catch (...) {
