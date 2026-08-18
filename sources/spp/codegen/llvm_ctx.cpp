8e97d1ad4eecfcec658032e196a0716fb337d1f8
SamG101-Developer <samuelgardner101@gmail.com>
2026-08-18 19:54:01 +0100

    Move attr

--- sources/spp/codegen/llvm_ctx.cpp
+++ sources/spp/codegen/llvm_ctx.cpp
@@ -1,5 +1,6 @@
 module;
 #include <spp/macros.hpp>
+#include <spp/codegen/llvm_passes.hpp>
 
 module spp.codegen.llvm_ctx;
 import spp.codegen.llvm_coros;
@@ -19,7 +20,8 @@ auto spp::codegen::LlvmCtx::NewCtx(Str const &module_name) -> Unique<LlvmCtx> {
   ctx->Module = MakeUnique<llvm::Module>(module_name, *ctx->Context);
   ctx->Module->setModuleIdentifier(module_name);
   ctx->Module->setSourceFileName(module_name);
-  ctx->Module->setTargetTriple(llvm::Triple("x86_64-pc-linux-gnu"));
+  ctx->Module->setTargetTriple(llvm::Triple(kTargetTriple));
+  ctx->Module->setDataLayout(HostDataLayoutString());
   return ctx;
 }
 
