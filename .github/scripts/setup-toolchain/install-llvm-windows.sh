cad17aaa71efb838926980916d9f3acf1bfd4994
SamG101-Developer <samuelgardner101@gmail.com>
2026-08-19 11:38:17 +0100

    Fix LLVM installations

--- .github/scripts/setup-toolchain/install-llvm-windows.sh
+++ .github/scripts/setup-toolchain/install-llvm-windows.sh
@@ -7,6 +7,7 @@ source .github/scripts/lib/verified-fetch.sh
 
 url="https://github.com/llvm/llvm-project/releases/download/${LLVM_WIN_TAG}/${LLVM_WIN_ASSET//+/%2B}"
 tmp="$(cygpath -u "$RUNNER_TEMP")"
+prefix="$(cygpath -u "$SPP_LLVM_WIN_PREFIX")"
 verified_fetch "$url" "$tmp/llvm.tar.xz" "$LLVM_WIN_SHA256"
-mkdir -p "$SPP_LLVM_WIN_PREFIX"
-tar xf "$tmp/llvm.tar.xz" -C "$SPP_LLVM_WIN_PREFIX" --strip-components=1
+mkdir -p "$prefix"
+tar xf "$tmp/llvm.tar.xz" -C "$prefix" --strip-components=1
