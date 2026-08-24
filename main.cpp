cd907c6ec2d42b042dbe2d15fbbbf359b357e409
SamG101-Developer <samuelgardner101@gmail.com>
2026-08-24 01:06:26 +0100

    Make binary act properly with command-line arguments

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ static auto print_stacktrace_for_sigsegv(void *user_data, const char *reason, bo
   backtrace_symbols_fd(array, static_cast<int>(size), 2);
 }
 
-auto main(const std::int32_t, char **argv) -> int {
+auto main(const std::int32_t argc, char **argv) -> int {
 #ifdef NDEBUG
   mi_option_disable(mi_option_show_stats);
   mi_option_disable(mi_option_verbose);
@@ -29,7 +29,6 @@ auto main(const std::int32_t, char **argv) -> int {
   // Temporary test code to test a small project.
   llvm::install_fatal_error_handler(print_stacktrace_for_sigsegv);
   std::filesystem::current_path(std::filesystem::absolute(argv[0]).parent_path().parent_path() / "project");
-  spp::cli::handle_run("rel");
-  // spp::cli::run_cli(argc, argv);
+  spp::cli::run_cli(argc, argv);
   return 0;
 }
