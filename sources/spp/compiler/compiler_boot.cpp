0a9c1c52dc0e10d59c297cc5b7ee3704d15bbaa8
SamG101-Developer <samuelgardner101@gmail.com>
2026-09-11 12:21:05 +0100

    Add unwind tables (no runtime changes)

--- sources/spp/compiler/compiler_boot.cpp
+++ sources/spp/compiler/compiler_boot.cpp
@@ -434,6 +434,7 @@ auto spp::compiler::CompilerBoot::_LinkTimeOptimize(
   FEATURE_GATE(MemoryStackProtect) { codegen::ApplyStackProtector(lto_module.get()); }
   FEATURE_GATE(MemoryStackProbe) { codegen::ApplyStackClashProtection(lto_module.get()); }
   FEATURE_GATE(MemoryStackSplit) { codegen::ApplySafeStack(lto_module.get()); }
+  codegen::ApplyUnwindTables(lto_module.get());
   if (not codegen::EmitObjectFile(lto_module.get(), utils::files::NativeString(object_file).c_str())) { return; }
 
   // A cross build stops at the object, no linking available for
