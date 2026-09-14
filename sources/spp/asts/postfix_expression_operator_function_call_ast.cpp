f56514d1db83a46163e1e7c9378be6fd5bbc4bd1
SamG101-Developer <samuelgardner101@gmail.com>
2026-09-14 02:33:40 +0100

    Clean

--- sources/spp/asts/postfix_expression_operator_function_call_ast.cpp
+++ sources/spp/asts/postfix_expression_operator_function_call_ast.cpp
@@ -232,14 +232,13 @@ auto spp::asts::PostfixExpressionOperatorFunctionCallAst::Stage7_AnalyseSemantic
 
   // Special case for GenOnce called as a coroutine => auto
   // move into the "Yield" type.
-  if (_OverloadInfo->Proto->TokFun->TokenType == lex::SppTokenType::KW_COR and not meta->PreventAutoGeneratorResume) {
+  if (_OverloadInfo->Proto->TokFun->TokenType == lex::SppTokenType::KW_COR) {
     // This needs to be any type that EXTENDS GenOnce, not
     // just GenOnce itself.
     auto [_, _, is_once] = analyse::utils::type_utils::GetGenAndYieldTypes(
       *_OverloadInfo->Proto->ReturnType, *sm->CurrentScope, *meta->PostfixExpressionLhs, "GenOnce collapse");
     _IsCoroAndAutoResume = is_once;
   }
-  meta->PreventAutoGeneratorResume = false;
 
   // Todo: Is this needed?
   const auto ret_type = InferType(sm, meta);
