a4b3dfae06457755db607d2f9db36a5f1801023b
SamG101-Developer <samuelgardner101@gmail.com>
2026-09-14 02:18:14 +0100

    Clean

--- sources/spp/parse/parser_spp.cpp
+++ sources/spp/parse/parser_spp.cpp
@@ -665,7 +665,7 @@ auto spp::parse::ParserSpp::parse_postfix_expression_op_early_return()
 
 auto spp::parse::ParserSpp::parse_postfix_expression_op_function_call()
   -> Unique<asts::PostfixExpressionOperatorFunctionCallAst> {
-  if (m_line_feed_ahead()) { return nullptr; }
+  if (_LineFeedAhead()) { return nullptr; }
   PARSE_OPTIONAL(p1, parse_generic_argument_group);
   PARSE_ONCE(p2, parse_function_call_argument_group);
   PARSE_OPTIONAL(p3, parse_fold_expression);
@@ -710,7 +710,7 @@ auto spp::parse::ParserSpp::parse_postfix_expression_op_keyword_res()
 
 auto spp::parse::ParserSpp::parse_postfix_expression_op_index()
   -> Unique<asts::PostfixExpressionOperatorIndexAst> {
-  if (m_line_feed_ahead()) { return nullptr; }
+  if (_LineFeedAhead()) { return nullptr; }
   PARSE_ONCE(p1, parse_token_left_square_bracket);
   PARSE_OPTIONAL(p2, parse_keyword_mut);
   PARSE_ONCE(p3, parse_expression);
@@ -720,7 +720,7 @@ auto spp::parse::ParserSpp::parse_postfix_expression_op_index()
 
 auto spp::parse::ParserSpp::parse_postfix_expression_op_slice()
   -> Unique<asts::PostfixExpressionOperatorSliceAst> {
-  if (m_line_feed_ahead()) { return nullptr; }
+  if (_LineFeedAhead()) { return nullptr; }
   PARSE_ONCE(p1, parse_token_left_square_bracket);
   PARSE_OPTIONAL(p2, parse_keyword_mut);
   PARSE_OPTIONAL(p3, parse_expression);
@@ -2840,7 +2840,7 @@ auto spp::parse::ParserSpp::parse_token_raw(const lex::RawTokenType tok, lex::Sp
       ? "the end of the file"s
       : "'"s + _Tokens[_Pos].data + "'";
 
-    if (m_store_error(_Pos, "Expected £, got "s + got)) {
+    if (_StoreError(_Pos, "Expected £, got "s + got)) {
       _ErrorBuilder->Tokens.insert(mapped_tok);
       return nullptr;
     }
@@ -2853,16 +2853,7 @@ auto spp::parse::ParserSpp::parse_token_raw(const lex::RawTokenType tok, lex::Sp
   return CREATE_AST(asts::TokenAst, pos, mapped_tok, _Tokens[_Pos - 1].data.data());
 }
 
-auto spp::parse::ParserSpp::m_line_feed_ahead() const
-  -> bool {
-  auto pos = _Pos;
-  while (pos < _TokensLen and _Tokens[pos].type == lex::RawTokenType::TK_SPACE) {
-    ++pos;
-  }
-  return pos < _TokensLen and _Tokens[pos].type == lex::RawTokenType::TK_LINE_FEED;
-}
-
-auto spp::parse::ParserSpp::m_store_error(
+auto spp::parse::ParserSpp::_StoreError(
   const std::size_t pos,
   Str &&err_str) const
   -> bool {
@@ -2873,6 +2864,15 @@ auto spp::parse::ParserSpp::m_store_error(
     return true;
   }
   return false;
+}
+
+auto spp::parse::ParserSpp::_LineFeedAhead() const
+  -> bool {
+  auto pos = _Pos;
+  while (pos < _TokensLen and _Tokens[pos].type == lex::RawTokenType::TK_SPACE) {
+    ++pos;
+  }
+  return pos < _TokensLen and _Tokens[pos].type == lex::RawTokenType::TK_LINE_FEED;
 }
 
 SPP_MOD_END
